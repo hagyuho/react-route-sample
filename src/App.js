5c1db5c3c85ff2c6bd9199e9e5bb4b90aecd3baf
gyuho <hagyuho90@gmail.com>
2021-12-01 10:05:14 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router";
 import { Link } from "react-router-dom";
-import About from "./component/About"; 
-import HistorySample from "./component/HistorySample";
 import Home from "./component/Home";
-import Profile from "./component/Profile";
-import Profiles from "./component/Profiles";
+// import About from "./component/About";
+// import HistorySample from "./component/HistorySample";
+// import Profile from "./component/Profile";
+// import Profiles from "./component/Profiles";
 
 const App = () => {
+  // const Home = React.lazy(() => import("./component/Home"));
+  const About = React.lazy(() => import("./component/About"));
+  const Profile = React.lazy(() => import("./component/Profile"));
+  const Profiles = React.lazy(() => import("./component/Profiles"));
+  const HistorySample = React.lazy(() => import("./component/HistorySample"));
+
   return (
     <div>
       <ul>
@@ -25,18 +31,17 @@ const App = () => {
         </li>
       </ul>
       <hr />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles" element={<Profiles />}>
-          <Route
-            path="/profiles"
-            element={<div>유저를 선택해주세요.</div>}
-          />
-          <Route path="/profiles/:username" element={<Profile />} />
-        </Route>
-        <Route path="history" element={<HistorySample/>}/>
-      </Routes>
+      <Suspense fallback={<div>loading</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profiles" element={<Profiles />}>
+            <Route path="/profiles" element={<div>유저를 선택해주세요.</div>} />
+            <Route path="/profiles/:username" element={<Profile />} />
+          </Route>
+          <Route path="history" element={<HistorySample />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
